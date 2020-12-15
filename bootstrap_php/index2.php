fc0ee4a243c1fcea61b7ba3cf6aaeed9c39e78fe
harik mohamed <med.harik@gmail.com>
2020-12-15 21:39:40 +0100

    les tableaux associatifs

---
+++ bootstrap_php/index2.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bootstrap & php</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"></head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+<h2 class="text-sm-center text-lg-left " >Liste des produits </h2>
+<div class="row">
+    <div class="col-3 text-center border my-2 ">
+        ` <img src="https://placehold.it/400x400" alt="" class="img-fluid rounded">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis necessitatibus at aperiam beatae accusamus, doloribus, non explicabo suscipit velit saepe architecto vero ipsam odio nemo quaerat ducimus laborum perferendis iusto?
+            
+            <br><a href=""  class="btn btn-primary">DETAILS</a>`
+    </div>
+    <div class="col-3 text-center border my-2 ">
+        ` <img src="https://placehold.it/400x400" alt="" class="img-fluid rounded">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis necessitatibus at aperiam beatae accusamus, doloribus, non explicabo suscipit velit saepe architecto vero ipsam odio nemo quaerat ducimus laborum perferendis iusto?
+            
+            <br><a href=""  class="btn btn-primary">DETAILS</a>`
+    </div>
+    <div class="col-3 text-center border my-2 ">
+        ` <img src="https://placehold.it/400x400" alt="" class="img-fluid rounded">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis necessitatibus at aperiam beatae accusamus, doloribus, non explicabo suscipit velit saepe architecto vero ipsam odio nemo quaerat ducimus laborum perferendis iusto?
+            
+            <br><a href=""  class="btn btn-primary">DETAILS</a>`
+    </div>
+    <div class="col-3 text-center border my-2 ">
+        ` <img src="https://placehold.it/400x400" alt="" class="img-fluid rounded">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis necessitatibus at aperiam beatae accusamus, doloribus, non explicabo suscipit velit saepe architecto vero ipsam odio nemo quaerat ducimus laborum perferendis iusto?
+            
+            <br><a href=""  class="btn btn-primary">DETAILS</a>`
+    </div>
+    <div class="col-3 text-center border my-2 ">
+        ` <img src="https://placehold.it/400x400" alt="" class="img-fluid rounded">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis necessitatibus at aperiam beatae accusamus, doloribus, non explicabo suscipit velit saepe architecto vero ipsam odio nemo quaerat ducimus laborum perferendis iusto?
+            
+            <br><a href=""  class="btn btn-primary">DETAILS</a>`
+    </div>
+    <div class="col-3 text-center border my-2 ">
+        ` <img src="https://placehold.it/400x400" alt="" class="img-fluid rounded">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis necessitatibus at aperiam beatae accusamus, doloribus, non explicabo suscipit velit saepe architecto vero ipsam odio nemo quaerat ducimus laborum perferendis iusto?
+            
+            <br><a href=""  class="btn btn-primary">DETAILS</a>`
+    </div>
+    <div class="col-3 text-center border my-2 ">
+        ` <img src="https://placehold.it/400x400" alt="" class="img-fluid rounded">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis necessitatibus at aperiam beatae accusamus, doloribus, non explicabo suscipit velit saepe architecto vero ipsam odio nemo quaerat ducimus laborum perferendis iusto?
+            
+            <br><a href=""  class="btn btn-primary">DETAILS</a>`
+    </div>
+    <div class="col-3 text-center border my-2 ">
+        ` <img src="https://placehold.it/400x400" alt="" class="img-fluid rounded">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis necessitatibus at aperiam beatae accusamus, doloribus, non explicabo suscipit velit saepe architecto vero ipsam odio nemo quaerat ducimus laborum perferendis iusto?
+            
+            <br><a href=""  class="btn btn-primary">DETAILS</a>`
+    </div>
+    <div class="col-3 text-center border my-2 ">
+        ` <img src="https://placehold.it/400x400" alt="" class="img-fluid rounded">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis necessitatibus at aperiam beatae accusamus, doloribus, non explicabo suscipit velit saepe architecto vero ipsam odio nemo quaerat ducimus laborum perferendis iusto?
+            
+            <br><a href=""  class="btn btn-primary">DETAILS</a>`
+    </div>
+    <div class="col-3 text-center border my-2 ">
+        ` <img src="https://placehold.it/400x400" alt="" class="img-fluid rounded">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis necessitatibus at aperiam beatae accusamus, doloribus, non explicabo suscipit velit saepe architecto vero ipsam odio nemo quaerat ducimus laborum perferendis iusto?
+            
+            <br><a href=""  class="btn btn-primary">DETAILS</a>`
+    </div>
+    <div class="col-3 text-center border my-2 ">
+        ` <img src="https://placehold.it/400x400" alt="" class="img-fluid rounded">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis necessitatibus at aperiam beatae accusamus, doloribus, non explicabo suscipit velit saepe architecto vero ipsam odio nemo quaerat ducimus laborum perferendis iusto?
+            
+            <br><a href=""  class="btn btn-primary">DETAILS</a>`
+    </div>
+    
+
+</div>
+          
+            
+            
+
+
+</div>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ tableau_associatif/tab1.php
@@ -0,0 +1,10 @@
+<?php 
+//les tableaux indexés  (les cases sont numérotées  à partir de l'indice 0)
+$t=['hp','dell','sony'];
+// echo $t[1];
+//les tableaux associatifs (cle=>valeur , key=>value) : les cases (valeurs) sont nommées cad associées à une clé 
+$a=['libelle'=>'hp dv 6','marque'=>'hp','prix'=>7000,'image'=>'hp.png'];
+echo $a['prix'];
+
+
+?>
