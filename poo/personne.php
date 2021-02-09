b36a8c5bc5307d4a4a1197917c1a6a74bc0cc6d3
harik mohamed <med.harik@gmail.com>
2021-02-09 21:27:48 +0100

    POO : constructeur, private et public (visibility),encapsulation

--- poo/personne.php
+++ poo/personne.php
@@ -1,22 +1,70 @@
 <?php
 class Personne
 {
+
+
+    //POO : 
+    // ENCAPSULATION  :
+    // (PROCTECTION DES DONNEES )
+    // comment : 
+    // en declarant private les attibuts
+    // et en autorisant leurs acces (lecture (get), ecriture (set))
+
+
     // les attributs 
     public $nom;
     public $prenom;
-    public $age;
+    private  $age;
+    public $genre;
+    // private   $pri;
+    // protected $pro;
+    /* modificateur d'accès : le degré  de visibilité*/
+    // le constructeur : une methode initialisant les attrbuts  de l'objet
+    public function __construct($nom = "", $prenom = "inconnu", $age = 0, $genre = "homme")
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->age = $age;
+        $this->genre = $genre;
+    }
+
+
+
 
     //les methodes / operations /message/ fonction/ programme
-    function afficher()
+    public function afficher()
     {
         echo  "le nom est " . $this->nom . ", prenom est " . $this->prenom . ", age est " . $this->age . "<br>";
     }
-    function majorite()
+    public function majorite()
     {
         if ($this->age > 18) {
-            echo "majeur<br>";
+            echo "majeur" . $this->hf() . "<br>";
         } else {
-            echo "mineur<br>";
+            echo "mineur" . $this->hf() . "<br>";
         }
+    }
+
+    private function  hf()
+    {
+        if ($this->genre == "homme") {
+            return "";
+        } else {
+            return "e";
+        }
+    }
+    public function   setAge($age)
+    {
+        if (is_numeric($age) && $age > 0) {
+
+            $this->age = $age;
+        } else {
+            echo "Age est incorrect<br>";
+        }
+    }
+
+    public function  getAge()
+    {
+        return $this->age;
     }
 }
--- poo/test.php
+++ poo/test.php
@@ -2,17 +2,27 @@
 include "personne.php";
 //instancier la classe personne pour creer des objets
 
-$ali = new Personne();
-$ali->nom = "ALAMI";
-$ali->prenom = "ALI";
-$ali->age = 25;
+$ali = new Personne("ALAMI", "ALI", 19, "homme");
+// $ali->nom = "ALAMI";
+// $ali->prenom = "ALI";
+// $ali->age = 25;
+// $ali->pri = 100;//erreur ?
 
 $ali->afficher();
 $ali->majorite();
 
-$aya = new Personne();
-$aya->nom = "ALAOUI";
-$aya->prenom = "AYA";
-$aya->age = 3;
+$aya = new Personne("ALAOUI", "AYA", 28, "femme");
+// $aya->nom = "ALAOUI";
+// $aya->prenom = "AYA";
+// $aya->age = 3;
+$aya->setAge("test");
 $aya->afficher();
 $aya->majorite();
+echo "age de aya est " . $aya->getAge();
+
+
+$amine = new Personne("AMINI", "AMINE", 56);
+
+$amine->afficher();
+$amine->majorite();
+// echo $amine->hf();
